e7b0876ec12ec60a88a75e88b1e3434c749e1914
George Kiagiadakis <kiagiadakis.george@gmail.com>
2009-07-12 19:34:37 +0300

    Add wrappers for gst_init().

---
+++ src/qgstglobal.cpp
@@ -0,0 +1,32 @@
+/*
+    Copyright (C) 2009  George Kiagiadakis <kiagiadakis.george@gmail.com>
+
+    This library is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "qgstglobal.h"
+#include <gst/gst.h>
+
+namespace QtGstreamer {
+
+void qGstInit()
+{
+    qGstInit(0, 0);
+}
+
+void qGstInit(int *argc, char **argv[])
+{
+    gst_init(argc, argv);
+}
+
+}
