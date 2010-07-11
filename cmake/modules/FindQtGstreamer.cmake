d9cddfd6281ba8b3ebd65b490befcfb5241140cc
George Kiagiadakis <kiagiadakis.george@gmail.com>
2010-07-11 17:44:09 +0300

    Improve the build system; add new features and examples: * Add ability to build QtGstreamer as a static library by passing -DSTATIC_QTGSTREAMER=1 to cmake. * Add a FindQtGstreamer.cmake script, which is installed as QtGstreamerConfig.cmake   together with a generated QtGstreamerTargets.cmake script, that can be used to   find QtGstreamer from external cmake-based projects. Both shared and static versions   of QtGstreamer are supported. * Add an uninstall target, copied from the KDE4 scripts. * Rewrite the CMakeLists.txt of the examples so that they can also be built standalone   and serve as examples of how to use QtGstreamer from external projects. * Add qmake project files for the examples, to act as examples for people using qmake.

---
+++ cmake/modules/FindQtGstreamer.cmake
@@ -0,0 +1,88 @@
+# - Try to find QtGstreamer
+# Once done this will define
+#
+#  QTGSTREAMER_FOUND - system has QtGstreamer
+#  QTGSTREAMER_INCLUDE_DIR - the QtGstreamer include directory
+#  QTGSTREAMER_INCLUDES - the include directories needed to use QtGstreamer
+#  QTGSTREAMER_LIBRARY - the QtGstreamer library
+#  QTGSTREAMER_LIBRARIES - the libraries needed to use QtGstreamer
+#  QTGSTREAMER_DEFINITIONS - definitions recommended for using QtGstreamer
+#  QTGSTREAMER_FLAGS - extra compiler switches recommended for using QtGstreamer
+#
+# Copyright (c) 2010, George Kiagiadakis <kiagiadakis.george@gmail.com>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+
+
+# if variables are already in cache or we are building QtGstreamer
+if((QTGSTREAMER_LIBRARY AND QTGSTREAMER_INCLUDE_DIR) OR BUILDING_QTGSTREAMER)
+    set(QtGstreamer_FIND_QUIETLY TRUE)
+else()
+    set(QtGstreamer_FIND_QUIETLY FALSE)
+endif()
+
+set(_QTGSTREAMER_LINK_TO_QTCORE_REQUIRED FALSE)
+
+if(BUILDING_QTGSTREAMER)
+    set(QTGSTREAMER_LIBRARY QtGstreamer)
+    set(QTGSTREAMER_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/src)
+else()
+    # Attempt to find the generated QtGstreamerTargets.cmake in the same directory
+    get_filename_component(_QTGSTREAMER_CONFIG_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+    find_file(_QTGSTREAMER_TARGETS_FILE QtGstreamerTargets.cmake PATHS ${_QTGSTREAMER_CONFIG_DIR} NO_DEFAULT_PATH)
+
+    if(_QTGSTREAMER_TARGETS_FILE-NOTFOUND)
+        # Targets file not found. Do a typical search for QtGstreamer.
+        # Normally, this path is never executed. It is just provided as a fallback in case something goes wrong.
+        find_library(QTGSTREAMER_LIBRARY QtGstreamer
+                     PATHS "${_QTGSTREAMER_CONFIG_DIR}/../../lib")
+        find_path(QTGSTREAMER_INCLUDE_DIR QGst/global.h
+                  PATHS "${_QTGSTREAMER_CONFIG_DIR}/../../include"
+                  PATH_SUFFIXES QtGstreamer)
+        set(_QTGSTREAMER_LINK_TO_QTCORE_REQUIRED TRUE)
+    else()
+        # Targets file found. Use imported QtGstreamer target and relative include path.
+        # We assume that this file has been installed in $PREFIX/lib/QtGstreamer/,
+        # so the include path should evaluate to $PREFIX/include/QtGstreamer
+        include(${_QTGSTREAMER_TARGETS_FILE})
+        set(QTGSTREAMER_LIBRARY QtGstreamer)
+        get_filename_component(QTGSTREAMER_INCLUDE_DIR "${_QTGSTREAMER_CONFIG_DIR}/../../include/QtGstreamer" ABSOLUTE)
+    endif()
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(QtGstreamer DEFAULT_MSG QTGSTREAMER_INCLUDE_DIR QTGSTREAMER_LIBRARY)
+
+if(QTGSTREAMER_FOUND)
+    # Find dependencies, if not already found
+    if (NOT DEFINED QT_INCLUDE_DIR)
+        message(STATUS "Qt hasn't been found yet. Looking...")
+        find_package(Qt4 COMPONENTS QtCore REQUIRED)
+    endif()
+
+    if (NOT DEFINED Boost_INCLUDE_DIRS)
+        message(STATUS "Boost hasn't been found yet. Looking...")
+        find_package(Boost REQUIRED)
+    endif()
+
+    # Set misc variables
+    set(QTGSTREAMER_INCLUDES ${QTGSTREAMER_INCLUDE_DIR} ${QT_INCLUDE_DIR} ${Boost_INCLUDE_DIRS})
+    set(QTGSTREAMER_DEFINITIONS "-DQT_NO_KEYWORDS")
+
+    if (_QTGSTREAMER_LINK_TO_QTCORE_REQUIRED)
+        set(QTGSTREAMER_LIBRARIES ${QTGSTREAMER_LIBRARY} ${QT_QTCORE_LIBRARY})
+    else()
+        set(QTGSTREAMER_LIBRARIES ${QTGSTREAMER_LIBRARY})
+    endif()
+
+    if (CMAKE_COMPILER_IS_GNUCXX)
+        include(TestCXXAcceptsFlag)
+        check_cxx_accepts_flag("-std=c++0x" _QTGSTREAMER_HAVE_CXX0X_SUPPORT)
+        if (_QTGSTREAMER_HAVE_CXX0X_SUPPORT)
+            set(QTGSTREAMER_FLAGS "-std=c++0x")
+        endif()
+    endif()
+endif()
+
+mark_as_advanced(QTGSTREAMER_LIBRARY QTGSTREAMER_INCLUDE_DIR)
---
+++ examples/echo/echo.pro
@@ -0,0 +1,27 @@
+# This is a qmake project file, provided as an example on how to use qmake with QtGstreamer.
+# There is nothing special about it, the only difference with a normal qmake project
+# is that you specify INCLUDEPATH and LIBS to point to QtGstreamer. Unfortunately, qmake
+# does not provide features like searching for libraries, so both the include path and the
+# library path have to be typed manually here. It is recommended to use cmake instead,
+# unless you have a reason to use qmake.
+
+TEMPLATE = app
+TARGET = echo
+
+# Change this to point to the location where the QtGstreamer headers are installed
+INCLUDEPATH += /usr/local/include/QtGstreamer
+
+# Change this to point to the location where the QtGstreamer library is installed
+LIBS += -L/usr/local/lib -lQtGstreamer
+
+# Recommended if you are using g++ 4.3 or later. Must be removed for other compilers.
+QMAKE_CXXFLAGS += -std=c++0x
+
+# Recommended, to avoid possible issues with the "emit" keyword
+DEFINES += QT_NO_KEYWORDS
+
+# This example has no GUI
+QT -= gui
+
+# Input
+SOURCES += main.cpp
