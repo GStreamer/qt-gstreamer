22dd34ef360322161a854a821771a13bf9a4d457
George Kiagiadakis <kiagiadakis.george@gmail.com>
2010-10-22 17:02:26 +0300

    Fix typo.

--- src/QGst/clock.cpp
+++ src/QGst/clock.cpp
@@ -1,6 +1,6 @@
 /*
     Copyright (C) 2010  Collabora Multimedia.
-      @author Mauricio Piacentini <mauricio.piacentini@collabora.co.u
+      @author Mauricio Piacentini <mauricio.piacentini@collabora.co.uk>
 
     This library is free software; you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published
