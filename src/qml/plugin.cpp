1790311bcf7b53ef2d36f8c72b2875edc66e4b33
George Kiagiadakis <george.kiagiadakis@collabora.com>
2012-01-16 01:33:01 +0200

    Add a qml plugin that exports GraphicsVideoWidget as a QML item called "VideoItem"

---
+++ src/qml/plugin.cpp
@@ -0,0 +1,37 @@
+/*
+    Copyright (C) 2012 Collabora Ltd. <info@collabora.com>
+      @author George Kiagiadakis <george.kiagiadakis@collabora.com>
+
+    This library is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "videoitem.h"
+#include <QtDeclarative/QDeclarativeExtensionPlugin>
+
+class QtGStreamerPlugin : public QDeclarativeExtensionPlugin
+{
+    Q_OBJECT
+public:
+    void registerTypes(const char *uri);
+};
+
+void QtGStreamerPlugin::registerTypes(const char *uri)
+{
+    qmlRegisterType<VideoItem>(uri, 0, 10, "VideoItem");
+    qmlRegisterUncreatableType<QGst::Ui::GraphicsVideoSurface>(uri, 0, 10, "GraphicsVideoSurface",
+        QLatin1String("Creating a QGst::Ui::GraphicsVideoSurface from QML is not supported"));
+}
+
+Q_EXPORT_PLUGIN2(qtgstPlugin, QtGStreamerPlugin)
+
+#include "plugin.moc"
