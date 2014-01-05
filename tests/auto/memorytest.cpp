e4b22f13af8b931e34bfd14fbcde8db380555a99
Diane Trout <diane@ghic.org>
2014-01-04 22:12:27 -0800

    tests/auto/memorytest.cpp verify src/QGst/memory.

---
+++ tests/auto/memorytest.cpp
@@ -0,0 +1,52 @@
+/*
+    Copyright (C) 2010  Diane Trout <diane@ghic.org>
+
+    This library is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "qgsttest.h"
+#include <QGlib/Error>
+#include <QGst/Memory>
+
+class MemoryTest : public QGstTest
+{
+    Q_OBJECT
+private Q_SLOTS:
+    void testMap();
+};
+
+void MemoryTest::testMap()
+{
+    QGst::MemoryPtr mem = QGst::Memory::create(100);
+    size_t offset;
+    size_t maxalloc;
+    size_t size = mem->getSizes(offset, maxalloc);
+
+    QCOMPARE(size, static_cast<unsigned long>(100));
+    QCOMPARE(offset, static_cast<unsigned long>(0));
+    QVERIFY(maxalloc >= 100);
+
+    QGst::MapInfo info;
+    QVERIFY(mem->map(info, QGst::MapRead));
+    QVERIFY(info.data != NULL);
+    QVERIFY(info.size == 100);
+    QVERIFY(info.maxsize == maxalloc);
+
+    mem->unmap(info);
+}
+
+QTEST_APPLESS_MAIN(MemoryTest)
+
+#include "moc_qgsttest.cpp"
+#include "memorytest.moc"
