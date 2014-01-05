3c3afa63fdc7bfa1d1a8546e74af8aa61cb3d334
Diane Trout <diane@ghic.org>
2014-01-04 22:07:34 -0800

    src/QGst/memory.{h,cpp} Provide minimal functional to wrap GstMemory objects.
    This just allows creating a GstMemory object, getting its size and
    mapping/unmapping it.
    
    The mapping functionality uses a small structure GstMapInfo to hold
    information abount and an accessor pointer to the raw memory.
    
    I created a new structure MapInfo which duplicates GstMapInfo because
    a goal of QGst was to allow building gstreamer applications without
    having gstreamer dev headers installed.
    
    Question: is there a better way of handling that structure?

---
+++ src/QGst/memory.cpp
@@ -0,0 +1,57 @@
+/*
+    Copyright (C) 2013  Diane Trout <diane@ghic.org
+
+    This library is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "memory.h"
+#include "buffer.h"
+
+#include <gst/gstobject.h>
+#include <gst/gstminiobject.h>
+#include <gst/gstallocator.h>
+#include <gst/gstmemory.h>
+#include <gst/gstbuffer.h>
+
+namespace QGst {
+
+MemoryPtr Memory::create(size_t size)
+{
+    return MemoryPtr::wrap(gst_allocator_alloc(NULL, size, NULL));
+}
+
+MemoryPtr Memory::create(MemoryFlags flags, void *allocator, MemoryPtr parent, size_t maxsize, size_t align, size_t offset, size_t size)
+{
+    MemoryPtr mem;
+
+    gst_memory_init(mem, static_cast<GstMemoryFlags>(static_cast<int>(flags)), static_cast<GstAllocator *>(allocator), parent, maxsize, align, offset, size);
+    return mem;
+}
+
+size_t Memory::getSizes(size_t &offset, size_t &maxsize)
+{
+    return gst_memory_get_sizes(object<GstMemory>(), &offset, &maxsize);
+}
+
+bool Memory::map(MapInfo &info, MapFlags flags)
+{
+    return gst_memory_map(object<GstMemory>(), reinterpret_cast<GstMapInfo *>(&info), static_cast<GstMapFlags>(static_cast<int>(flags)));
+}
+
+void Memory::unmap(MapInfo &info)
+{
+    gst_memory_unmap(object<GstMemory>(), reinterpret_cast<GstMapInfo *>(&info));
+}
+
+} // namespace QGst
+
---
+++ src/QGst/memory.h
@@ -0,0 +1,60 @@
+/*
+    Copyright (C) 2013  Diane Trout <diane@ghic.org
+
+    This library is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef QGST_MEMORY_H
+#define QGST_MEMORY_H
+
+#include "global.h"
+#include "miniobject.h"
+
+namespace QGst {
+class Buffer;
+
+    /*! \headerfile memory.h <QGst/Memory>
+     *  \brief Wrapper class for GstMemory
+     *
+     * GstMemory is a lightweight refcounted object that wraps a region
+     * of memory. They are typically used to manage the data of a
+     * GstBuffer.
+     */
+
+struct MapInfo
+{
+    void *memory;
+    MapFlags flags;
+    quint8 *data;
+    size_t size;
+    size_t maxsize;
+};
+
+class QTGSTREAMER_EXPORT Memory : public MiniObject
+{
+    QGST_WRAPPER(Memory)
+public:
+    static MemoryPtr create(size_t size);
+    static MemoryPtr create(MemoryFlags flags, void *allocator, MemoryPtr parent, size_t maxsize, size_t align, size_t offset, size_t size);
+
+    size_t getSizes(size_t &offset, size_t &maxsize);
+
+    bool map(MapInfo &info, MapFlags flags);
+    void unmap(MapInfo &info);
+};
+
+} // namespace QGst
+
+QGST_REGISTER_TYPE(QGst::Memory)
+
+#endif
