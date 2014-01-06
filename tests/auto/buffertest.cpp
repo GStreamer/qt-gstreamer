076d5f7ff750063acef72fee67375d2f47e6d68b
Diane Trout <diane@ghic.org>
2014-01-05 22:15:36 -0800

    tests/auto/buffertest.cpp additional tests of buffer memory extract & peek

--- tests/auto/buffertest.cpp
+++ tests/auto/buffertest.cpp
@@ -17,6 +17,7 @@
 */
 #include "qgsttest.h"
 #include <QGst/Buffer>
+#include <QGst/Memory>
 #include <QGst/Caps>
 
 class BufferTest : public QGstTest
@@ -26,6 +27,7 @@ private Q_SLOTS:
     void simpleTest();
     void flagsTest();
     void copyTest();
+    void memoryPeekTest();
 };
 
 void BufferTest::simpleTest()
@@ -68,6 +70,24 @@ void BufferTest::copyTest()
     QVERIFY(buffer->flags() != buffer2->flags());
 }
 
+void BufferTest::memoryPeekTest()
+{
+    QGst::BufferPtr buffer = QGst::Buffer::create(10);
+    guint8 bytes[100];
+    size_t returned_bytes;
+
+    returned_bytes = buffer->extract(0, &bytes, 10);
+    QCOMPARE(returned_bytes, static_cast<size_t>(10));
+
+    returned_bytes = buffer->extract(0, &bytes, 20);
+    QCOMPARE(returned_bytes, static_cast<size_t>(10));
+
+    QGst::MemoryPtr m = buffer->peekMemory(0);
+
+    QVERIFY(m);
+    QVERIFY(m->isWritable());
+
+}
 QTEST_APPLESS_MAIN(BufferTest)
 
 #include "moc_qgsttest.cpp"
