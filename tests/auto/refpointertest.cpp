203f8d7fa4cad48f445561162e220ca99fe357d6
Diane Trout <diane@ghic.org>
2014-01-05 22:41:19 -0800

    tests/auto/refpointertest.cpp playbin2 was renamed to playbin

--- tests/auto/refpointertest.cpp
+++ tests/auto/refpointertest.cpp
@@ -132,7 +132,7 @@ void RefPointerTest::dynamicCastIfaceToObjectTest()
 
 void RefPointerTest::cppWrappersTest()
 {
-    QGst::ElementPtr e = QGst::ElementFactory::make("playbin2");
+    QGst::ElementPtr e = QGst::ElementFactory::make("playbin");
     QVERIFY(!e.isNull());
 
     {
