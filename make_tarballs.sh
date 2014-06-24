0cad07f61f3ca05e5eee4343409ba85cbdeab9cd
George Kiagiadakis <george.kiagiadakis@collabora.com>
2014-06-24 13:26:32 +0300

    make_tarballs.sh: modify to generate .tar.xz archives and sha256sum files, like other gst modules

--- make_tarballs.sh
+++ make_tarballs.sh
@@ -8,10 +8,7 @@
 VERSION=`cat CMakeLists.txt | grep QTGSTREAMER_VERSION | sed -r 's#set\(QTGSTREAMER_VERSION ([0-9\.]+)\)#\1#'`
 echo "Making tarballs for version $VERSION..."
 
-git archive --format=tar --prefix=qt-gstreamer-$VERSION/ HEAD | gzip -9 > qt-gstreamer-$VERSION.tar.gz
-md5sum qt-gstreamer-$VERSION.tar.gz > qt-gstreamer-$VERSION.tar.gz.md5
-
-git archive --format=tar --prefix=qt-gstreamer-$VERSION/ HEAD | bzip2 -9 > qt-gstreamer-$VERSION.tar.bz2
-md5sum qt-gstreamer-$VERSION.tar.bz2 > qt-gstreamer-$VERSION.tar.bz2.md5
+git archive --format=tar --prefix=qt-gstreamer-$VERSION/ HEAD | xz > qt-gstreamer-$VERSION.tar.xz
+sha256sum qt-gstreamer-$VERSION.tar.xz > qt-gstreamer-$VERSION.tar.xz.sha256sum
 
 echo "Done."
