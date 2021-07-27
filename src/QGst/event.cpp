3edf06f7d15f97a8e87fca8148640946a12a5f9e
oper <oper@localhost.local>
2021-07-27 13:30:51 +0400

    Compilation error. Incorrect type conversion in the function.

--- src/QGst/event.cpp
+++ src/QGst/event.cpp
@@ -125,7 +125,7 @@ Segment SegmentEvent::segment() const
 //********************************************************
 TagEventPtr TagEvent::create(const TagList & taglist)
 {
-    GstEvent * e = gst_event_new_tag(gst_tag_list_copy(taglist));
+    GstEvent * e = gst_event_new_tag(gst_tag_list_copy(&taglist));
     return TagEventPtr::wrap(e, false);
 }
 
--- src/QGst/message.cpp
+++ src/QGst/message.cpp
@@ -157,7 +157,7 @@ QString InfoMessage::debugMessage() const
 
 TagMessagePtr TagMessage::create(const ObjectPtr & source, const TagList & taglist)
 {
-    GstMessage *m = gst_message_new_tag(source, gst_tag_list_copy(taglist));
+    GstMessage *m = gst_message_new_tag(source, gst_tag_list_copy(&taglist));
     return TagMessagePtr::wrap(m, false);
 }
 
